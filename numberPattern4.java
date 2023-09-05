a012dfa42c1d9db903255d8d5ef7c7af65159f14
Ma-b2020 <93130732+Ma-b2020@users.noreply.github.com>
2023-09-05 18:49:29 +0530

    Create numberPattern4.java

---
+++ numberPattern4.java
@@ -0,0 +1,29 @@
+package patternPractice;
+
+public class numPattern4 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter n value:");
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++){
+			for(int j=1;j<=i;j++){
+				System.out.print(j+" "); 
+			}
+			for(int j=i-1;j>=1;j--){
+				System.out.print(j+ " "); 
+			}
+			System.out.println();
+		}
+	}
+}
+
+
+/*
+OUTPUT-
+Enter n value:5
+1 
+1 2 1 
+1 2 3 2 1 
+1 2 3 4 3 2 1 
+1 2 3 4 5 4 3 2 1 
+*/
